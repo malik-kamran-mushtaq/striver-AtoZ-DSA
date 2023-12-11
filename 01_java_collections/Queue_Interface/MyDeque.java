f108a4880cab9178af598c61f95a279bc9aa0f17
Malik Kamran Mushtaq <114479777+malik-kamran-mushtaq@users.noreply.github.com>
2023-12-11 12:27:43 +0000

    added Queue interface. Priority Queue and Deque(ArrayDeque)

---
+++ 01_java_collections/Queue_Interface/MyDeque.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MyDeque {
+    public static void main(String[] args){
+        Deque<String> deque = new ArrayDeque<String>();
+        deque.add("Kamran");
+        deque.add("Owais");
+        deque.add("Shahid");
+        deque.add("Owais");
+        deque.add("Yahya");
+        deque.addFirst("Danish");
+        deque.addLast("Basit");
+
+        for(String str : deque) {
+            System.out.println(str);
+        }
+
+        System.out.println("After Removing: ");
+        deque.removeFirst();
+        deque.removeLast();
+
+        for(String str : deque) {
+            System.out.println(str);
+        }
+    }
+}
---
+++ 01_java_collections/Queue_Interface/MyPriorityQueue.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class MyPriorityQueue {
+    public static void main(String[] args){
+        PriorityQueue<String> q = new PriorityQueue<String>();
+        q.add("Waseem");
+        q.add("Turab");
+        q.add("Zehan");
+        q.add("Danish");
+        q.add("Burhan");
+
+        System.out.println("Head element: "+q.element());  //automatically takes the priority of alphabetical orer
+        System.out.println("Head element: "+q.peek());
+
+        System.out.println("First Iterator::");
+        Iterator itr = q.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        q.remove();
+        q.poll();
+
+        System.out.println("After Removing::");
+        Iterator itr2 = q.iterator();
+        while(itr2.hasNext()) {
+            System.out.println(itr2.next());
+        } 
+    }
+}
